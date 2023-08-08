c92f5671479e51fb727ea92f4881dd861a98db8b
Dmitriy Panteleev <dpanteleev@tradingview.com>
2023-08-08 12:06:16 +0300

    add some classes

--- uno-ng/uno.py
+++ uno-ng/uno.py
@@ -21,7 +21,48 @@ class Deck:
     def show_deck(self):
         print(self.play_deck)
 
+# Player class
+class Player:
+    def __init__(self, name, deck):
+        self.name = name
+        self.hand = []
+        for i in range(6):
+            self.draw_card(deck)
+
+    def draw_card(self, deck):
+        self.hand.append(deck.pop())
+
+    def show_hand(self):
+        print(self.hand)
+
+# Playtable class
+class Playtable:
+    def __init__(self, card):
+        self.tablecard = card
+    
+    def show_playtable(self):
+        print(self.tablecard)
+    
+    def check_play_card(self, card):
+        if card[0] == self.tablecard[0] or card[1] == self.tablecard[1]:
+            return True
+        else:
+            return False
+
 if __name__ == '__main__':
     deck = Deck()
+    print("New deck created")
+    print(f"Deck has {len(deck.play_deck)} cards:")
+    deck.show_deck()
+
+    players = []
+    for i in range(6):
+        players.append(Player(f'Player {i}', deck.play_deck))
+
+    for player in players:
+        print(f"{player.name} has {len(player.hand)} cards:")
+        player.show_hand()
+    
+    print("Deck after dealing:")
     print(f"Deck has {len(deck.play_deck)} cards:")
     deck.show_deck()
